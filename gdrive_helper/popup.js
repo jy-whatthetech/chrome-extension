98cd5af7f9e70abefa412a7443f8faebd2a6feec
jy-whatthetech <james.yeh1@gmail.com>
2020-11-20 15:32:34 -0800

    more styling cleanup; hook up prefix/suffix/copyCount with storage

--- gdrive_helper/popup.js
+++ gdrive_helper/popup.js
@@ -1,52 +1,49 @@
 const PREFIX_ID = "namePrefix";
 const SUFFIX_ID = "nameSuffix";
 const COPYCOUNT_ID = "copyCount";
-const SHARE_LINKS_KEY = "shareLinks";
+const SHARE_LINKS_ID = "shareLinks";
 
 document.addEventListener("DOMContentLoaded", documentEvents, false);
 
 function documentEvents() {
+  const copyCountElement = document.getElementById(COPYCOUNT_ID);
+  const prefixElement = document.getElementById(PREFIX_ID);
+  const suffixElement = document.getElementById(SUFFIX_ID);
+
   chrome.storage.local.get([PREFIX_ID, SUFFIX_ID, COPYCOUNT_ID], function(
     result
   ) {
-    if (result[PREFIX_ID]) {
-      const val = parseInt(result.commentsToDisplay);
-      document.getElementById("commentsSlider").value = val;
-      document.getElementById(
-        "commentsLabel"
-      ).innerText = `Number of comments to display: ${val}`;
-    }
+    const copyCount = result[COPYCOUNT_ID];
+    const prefix = result[PREFIX_ID];
+    const suffix = result[SUFFIX_ID];
 
-    if (result.ratingsToFilter) {
-      const val = parseInt(result.ratingsToFilter);
-      document.getElementById("ratingsSlider").value = val;
-      document.getElementById(
-        "ratingsLabel"
-      ).innerText = `Filter out videos below: ${val}%`;
+    if (copyCount) {
+      copyCountElement.value = copyCount;
+    }
+    if (prefix) {
+      prefixElement.value = prefix;
+    }
+    if (suffix) {
+      suffixElement.value = suffix;
     }
   });
 
-  document
-    .getElementById("commentsSlider")
-    .addEventListener("input", function() {
-      const val = document.getElementById("commentsSlider").value;
-
-      chrome.storage.local.set({ commentsToDisplay: val }, function() {});
-
-      document.getElementById(
-        "commentsLabel"
-      ).innerText = `Number of comments to display: ${val}`;
-    });
-
-  document
-    .getElementById("ratingsSlider")
-    .addEventListener("input", function() {
-      const val = document.getElementById("ratingsSlider").value;
-
-      chrome.storage.local.set({ ratingsToFilter: val }, function() {});
-
-      document.getElementById(
-        "ratingsLabel"
-      ).innerText = `Filter out videos below: ${val}%`;
-    });
+  copyCountElement.addEventListener("input", function() {
+    chrome.storage.local.set(
+      { [COPYCOUNT_ID]: copyCountElement.value },
+      function() {}
+    );
+  });
+  prefixElement.addEventListener("input", function() {
+    chrome.storage.local.set(
+      { [PREFIX_ID]: prefixElement.value },
+      function() {}
+    );
+  });
+  suffixElement.addEventListener("input", function() {
+    chrome.storage.local.set(
+      { [SUFFIX_ID]: suffixElement.value },
+      function() {}
+    );
+  });
 }
