4bc8e4da305ee596a694f9b2b12f78880194aa0e
jy-whatthetech <james.yeh1@gmail.com>
2020-11-22 11:20:18 -0800

    add sharedLinks textarea that shows when copy button is clicked

--- gdrive_helper/popup.js
+++ gdrive_helper/popup.js
@@ -1,7 +1,8 @@
 const PREFIX_ID = "namePrefix";
 const SUFFIX_ID = "nameSuffix";
 const COPYCOUNT_ID = "copyCount";
-const SHARE_LINKS_ID = "shareLinks";
+const SHARE_LINKS_ID = "sharedLinks";
+const COPY_BUTTON_ID = "copyButton";
 
 document.addEventListener("DOMContentLoaded", documentEvents, false);
 
@@ -9,6 +10,8 @@ function documentEvents() {
   const copyCountElement = document.getElementById(COPYCOUNT_ID);
   const prefixElement = document.getElementById(PREFIX_ID);
   const suffixElement = document.getElementById(SUFFIX_ID);
+  const sharedLinksElement = document.getElementById(SHARE_LINKS_ID);
+  const copyButton = document.getElementById(COPY_BUTTON_ID);
 
   chrome.storage.local.get([PREFIX_ID, SUFFIX_ID, COPYCOUNT_ID], function(
     result
@@ -45,5 +48,10 @@ function documentEvents() {
       { [SUFFIX_ID]: suffixElement.value },
       function() {}
     );
+  });
+
+  copyButton.addEventListener("click", (event) =>{
+    sharedLinksElement.style.display = "block";
+    event.preventDefault();
   });
 }
